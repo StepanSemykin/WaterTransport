663aa76b7cf80425aadf5079e67e8a5ceb10348e
Dmitri <gorsh03@gmail.com>
2025-11-03 19:43:40 +0400

    +tokenDTo

---
+++ Server/WaterTransportService.Api/DTO/TokenDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WaterTransportService.Api.DTO;
+
+public class LoginDto
+{
+    [Required, MaxLength(20)]
+    public string Phone { get; set; } = default!;
+    [Required, MinLength(6)]
+    public string Password { get; set; } = default!;
+}
+
+// Новые DTO для аутентификации
+public record RegisterDto(
+    [Required, MaxLength(20)] string Phone,
+    [Required, MinLength(6)] string Password
+);
+
+public record LoginResponseDto(
+    string AccessToken,
+    string RefreshToken,
+    UserDto User
+);
+
+public record RefreshTokenResponseDto(
+    string AccessToken,
+    string RefreshToken
+);
